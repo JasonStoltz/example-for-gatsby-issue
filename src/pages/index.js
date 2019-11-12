1a54162958e5ae89a758089d3e3c24116c65b101
Jason Stoltzfus <jastoltz24@gmail.com>
2019-11-12 09:08:50 -0500

    Added Search UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,37 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector"
+import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui"
+import { Layout as SearchLayout } from "@elastic/react-search-ui-views"
+
+import "@elastic/react-search-ui-views/lib/styles/styles.css"
+
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-371auk61r2bwqtdzocdgutmg",
+  engineName: "search-ui-examples",
+  hostIdentifier: "host-2376rb",
+})
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SearchProvider
+      config={{
+        apiConnector: connector,
+      }}
+    >
+      <div className="App">
+        <SearchLayout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
